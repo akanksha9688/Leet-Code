7852958befa9ef62e5a1f901c70750c3ccf42517
Akanksha Srivastava <112632617+akanksha9688@users.noreply.github.com>
2025-02-01 19:33:03 +0530

    Time: 0 ms (100%), Space: 18 MB (12.54%) - LeetHub

---
+++ 3429-special-array-i/3429-special-array-i.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isArraySpecial(self, nums: List[int]) -> bool:
+        for i in range(len(nums) - 1):
+            if (nums[i] + nums[i + 1]) % 2 == 0:
+                return False  # Break early if any consecutive pair has an even sum
+        return True  # Return True if all pairs have an odd sum
+        

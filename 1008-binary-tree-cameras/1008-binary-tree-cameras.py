43806962df0fe23935b6ff9a3dd0e4c470c3f31d
Akanksha Srivastava <112632617+akanksha9688@users.noreply.github.com>
2025-07-02 21:01:15 +0530

    Time: 1 ms (57.36%), Space: 18.2 MB (47.9%) - LeetHub

---
+++ 1008-binary-tree-cameras/1008-binary-tree-cameras.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minCameraCover(self, root: Optional[TreeNode]) -> int:
+        self.cnt=0
+        if self.dfs(root)==0:
+            self.cnt+=1
+        return self.cnt
+    def dfs(self,node):
+        if node is None:
+            return 1
+        left=self.dfs(node.left)
+        right=self.dfs(node.right)
+
+        if left==0 or right ==0:
+            self.cnt+=1
+            return 2
+        
+        if left ==2 or right ==2:
+            return 1
+        return 0
+        
